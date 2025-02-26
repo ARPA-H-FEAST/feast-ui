21140ad956f2ad4d18d89e62eeedb08ad5fd7fc3
Pat McNeely <pmcneely@gwu.edu>
2025-02-26 12:15:36 -0500

    Retain config information within the SPA internally

---
+++ src/configs/app_initial_config.js
@@ -0,0 +1,57 @@
+export function appConfig() {
+    return {
+    "dataversion": "1.0",
+    "logo": "FEAST",
+    "search_examples": "Example queries: nbcc, gw, prostate",
+    "webroot": "/gw-feast",
+    "headerlinks": [
+        {
+            "id": "home",
+            "label": "Home",
+            "url": "/gw-feast"
+        },
+        {
+            "id": "faq",
+            "label": "FAQ",
+            "url": "/gw-feast/static/faq"
+        },
+        {
+            "id": "about",
+            "label": "About",
+            "url": "/gw-feast/static/about"
+        }
+    ],
+    "fileuploadformats": [],
+    "fileuploadqc": [],
+    "footer": {
+        "links": [
+            {
+                "label": "License",
+                "url": "/gw-feast/static/license"
+            },
+            {
+                "label": "Privacy Policy",
+                "url": "/gw-feast/static/privacy/"
+            },
+            {
+                "label": "Disclaimer",
+                "url": "/gw-feast/static/disclaimer/"
+            },
+            {
+                "label": "Contact Us",
+                "url": "/gw-feast/static/contactus/"
+            }
+        ],
+        "license": "",
+        "funding": "Funded by ....",
+        "logos": [
+            "/imglib/logo-gwu.png",
+            "/imglib/temple.png",
+            "/imglib/embleema.png"
+        ]
+    },
+    "versionlist": [
+        "1.0"
+    ]
+  }
+}
