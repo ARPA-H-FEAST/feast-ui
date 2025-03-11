f186273647069684db20edc6f75de449f5f9599d
Pat McNeely <pmcneely2@gmail.com>
2025-03-11 08:58:08 -0400

    WIP - Update API backend endpoints

--- src/components/local_config.jsx
+++ src/components/local_config.jsx
@@ -1,26 +1,26 @@
 //import * as Zulamodule from "./module_exports";
 
 export const apiHash = {
-    dataset_init:process.env.REACT_APP_API_URL + "/dataset/init",
-    dataset_search:process.env.REACT_APP_API_URL + "/dataset/search",
-    dataset_list:process.env.REACT_APP_API_URL + "/dataset/list",
-    dataset_detail:process.env.REACT_APP_API_URL + "/dataset/detail",
-    dataset_image:process.env.REACT_APP_API_URL + "/dataset/image",
-    dataset_curation:process.env.REACT_APP_API_URL + "/dataset/curation",
-    dataset_savecuration:process.env.REACT_APP_API_URL + "/dataset/savecuration",
-    dataset_download:process.env.REACT_APP_API_URL + "/dataset/download",
-    dataset_upload:process.env.REACT_APP_API_URL + "/dataset/upload",
-    dataset_submit:process.env.REACT_APP_API_URL + "/dataset/submit",
-    dataset_glycan_finder:process.env.REACT_APP_API_URL + "/dataset/glycan_finder",
-    dataset_history_list:process.env.REACT_APP_API_URL + "/dataset/historylist",
-    dataset_history_detail:process.env.REACT_APP_API_URL + "/dataset/historydetail",
-    dataset_static_content:process.env.REACT_APP_API_URL + "/dataset/pagecn",
+    dataset_init: process.env.REACT_APP_API_URL + "/init/",
+    dataset_search: process.env.REACT_APP_API_URL + "/search/",
+    dataset_list: process.env.REACT_APP_API_URL + "/list/",
+    dataset_detail: process.env.REACT_APP_API_URL + "/detail/",
+    dataset_image: process.env.REACT_APP_API_URL + "/image/",
+    dataset_curation: process.env.REACT_APP_API_URL + "/curation/",
+    dataset_savecuration: process.env.REACT_APP_API_URL + "/savecuration/",
+    dataset_download: process.env.REACT_APP_API_URL + "/download/",
+    dataset_upload: process.env.REACT_APP_API_URL + "/upload/",
+    dataset_submit: process.env.REACT_APP_API_URL + "/submit/",
+    dataset_glycan_finder: process.env.REACT_APP_API_URL + "/glycan_finder/",
+    dataset_history_list: process.env.REACT_APP_API_URL + "/historylist/",
+    dataset_history_detail: process.env.REACT_APP_API_URL + "/historydetail/",
+    dataset_static_content: process.env.REACT_APP_API_URL + "/pagecn/",
     user_login_direct:process.env.REACT_APP_SMART_URL + "/users/login/",
     user_logout:process.env.REACT_APP_SMART_URL + "/users/logout/",
     user_info:process.env.REACT_APP_SMART_URL + "/users/whoami/",
     // init_info:process.env.REACT_APP_SMART_URL + "/users/init/",
     oauth_user_info:process.env.REACT_APP_INTERNAL_OAUTH_API_URL + "/userinfo/",
-    fhir_endpoint:process.env.REACT_APP_SMART_URL + "/api/fhir-query/"
+    fhir_endpoint: process.env.REACT_APP_SMART_URL + "/api/"
 };
 
 
