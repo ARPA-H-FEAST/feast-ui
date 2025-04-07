323f601eedd9052b0cd598e9cd5f685b04390dfe
Pat McNeely <pmcneely@gwu.edu>
2025-04-07 09:59:07 -0400

    Improve robustness of the UI to outages

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -16,5 +16,5 @@ chmod -R 744 build
 
 docker build -t feast-prototype-ui .
 
-docker run -d --rm --name ui-test -p 127.0.0.1:4242:80 feast-prototype-ui
+docker run -d --restart always --name ui-test -p 127.0.0.1:4242:80 feast-prototype-ui
 
