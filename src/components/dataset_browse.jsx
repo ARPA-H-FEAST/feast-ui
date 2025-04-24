284157e343a24a38e67978930cb47e17a66a6f31
Pat McNeely <pmcneely@gwu.edu>
2025-04-24 09:25:31 -0400

    WIP - Move state from dataset browser to zustand. FC update from class component.

--- src/components/dataset_browse.jsx
+++ src/components/dataset_browse.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom/cjs/react-router-dom";
 import Searchbox from "./search_box";
 import Filter from "./filter";
@@ -12,266 +12,260 @@ import {getColumns} from "./columns";
 import { Markup } from 'interweave';
 import { Card, CardContent } from "@mui/material";
 
-class DatasetBrowse extends Component {  
+import { useUserStore } from "../store/userStore";
+
+export default function DatasetBrowse(props) {  
   
-  state = {
-	  filterlist: [],
-    objlist:[],
-    statobj:{},
-    pageIdx:1,
-    pageBatchSize:5,
-    pageStartIdx:1,
-    pageEndIdx:5,
-    listid:"",
-    isLoaded:false,
-    searchquery:"",
-    dialog:{
-      status:false, 
-      msg:""
-    }
+  const [state, setState] = useState({
+      filterlist: [],
+      objlist:[],
+      statobj:{},
+      pageIdx:1,
+      pageBatchSize:5,
+      pageStartIdx:1,
+      pageEndIdx:5,
+      listid:"",
+      isLoaded:false,
+      searchquery:"",
+      dialog:{
+        status:false, 
+        msg:""
+      }
+    })
+
+  // state = {
+	//   filterlist: [],
+  //   objlist:[],
+  //   statobj:{},
+  //   pageIdx:1,
+  //   pageBatchSize:5,
+  //   pageStartIdx:1,
+  //   pageEndIdx:5,
+  //   listid:"",
+  //   isLoaded:false,
+  //   searchquery:"",
+  //   dialog:{
+  //     status:false, 
+  //     msg:""
+  //   }
+  // };
+
+const userCredentials = useUserStore((state) => state.userCredentials)
+const userIDTokenValues = useUserStore((state) => state.userIDTokenDetails)
+const storedUserInfo = useUserStore((state) => state.userInfo)
+
+useEffect(() => {
+  // console.log("********* MOUNT TRIGGERED *********")
+  const queryParameters = new URLSearchParams(window.location.search);
+  var searchQuery = (queryParameters.get("query") === null ? "" : queryParameters.get("query"))
+  setState({searchquery: searchQuery});
+  updateData("");
+  }, [])
+
+const updateData = async (searchQuery) => {
+
+  const access_csrf = localStorage.getItem("access_csrf")
+  // const credentials = JSON.parse(localStorage.getItem('userCredentials'))
+  // const id_token_values = JSON.parse(localStorage.getItem('userIDTokenValues'))
+  if (!userCredentials || !userCredentials.access_token) {
+    console.log("No user credentials located, redirecting")
+    // This doesn't work yet, but at least renders *something*
+    // see https://stackoverflow.com/a/45090151 for a possible workaround
+    return <Redirect to='/login' />
+  }
+  // XXX console.log("---> ID Tokens: " + userIDTokenValues + " (credentials: " + JSON.stringify(userCredentials) + ")")
+  const auth_url = userIDTokenValues.iss + "/userinfo/"
+
+  var reqObj = { "query": searchQuery };
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': access_csrf,
+      'Authorization': 'Bearer: ' + credentials.access_token,
+      'Iss-Oauth': auth_url,  // This address confirms login state on the backend
+    },
+    body: JSON.stringify(reqObj),
+    credentials: 'include'
   };
-
-  componentDidMount() {
-		const queryParameters = new URLSearchParams(window.location.search);
-    var searchQuery = (queryParameters.get("query") === null ? "" : queryParameters.get("query"))
-    this.setState({searchquery: searchQuery});
-		this.updateData("");
-   }
-
-
-  
-	updateData(searchQuery) {
- 
-    const access_csrf = localStorage.getItem("access_csrf")
-    const credentials = JSON.parse(localStorage.getItem('userCredentials'))
-    const id_token_values = JSON.parse(localStorage.getItem('userIDTokenValues'))
-    if (!credentials || !credentials.access_token) {
-      console.log("No user credentials located, redirecting")
-      // This doesn't work yet, but at least renders *something*
-      // see https://stackoverflow.com/a/45090151 for a possible workaround
-      return <Redirect to='/login' />
-    }
-    const auth_url = id_token_values.iss + "/userinfo/"
-
-    // console.log("---> Authorization: Bearer: " + credentials.access_token)
-    // console.log("---> Authorization API endpoint: " + auth_url)
-    var reqObj = { "query": searchQuery };
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': access_csrf,
-        'Authorization': 'Bearer: ' + credentials.access_token,
-        'Iss-Oauth': auth_url,  // This address confirms login state on the backend
-      },
-      body: JSON.stringify(reqObj),
-      credentials: 'include'
-    };
-    const svcUrl = LocalConfig.apiHash.dataset_search;
-    fetch(svcUrl, requestOptions)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          // console.log("RRR:", result);
-    // var tmpState = this.state;
-    // tmpState.isLoaded = true;          
-          if (result.status !== 0) {
-            // Set 'success' to zero
-            // Error handling here
-            this.setState({
-              dialog: { status: true, msg: result.msg },
-              objlist: []
-            })
-          }
-          this.setState({
-            objlist: result.recordlist,
-            statobj: result.stats
-          })
-        },
-      )
-      .catch((error) => {
-        console.log("Error on load: " + error)
-        this.setState(
-          {
-            isLoaded: false,
-            dialog: { msg: error, status: false }
-          })
-      }
-      )
+  const svcUrl = LocalConfig.apiHash.dataset_search;
+  const response = await fetch(svcUrl, requestOptions)
+  if (!response.ok) {
+    // Error handling
+    console.log("Error on data collection")
   }
-
-
-  handleDialogClose = () => {
-    var tmpState = this.state;
-    tmpState.dialog.status = false;
-    this.setState(tmpState);
-  }
-
-  
-  	handleKeyPress = (e) => {
-   	if(e.key === "Enter"){
-      	e.preventDefault();
-      	this.handleSearch();
-    	}
-  	}
- 
-
-	handleSearch = () => {
-		var searchQuery = ($("#query").val() === undefined ? this.state.searchquery : $("#query").val());
-    	this.updateData(searchQuery)
-    }
-
-
-
-    handleFilterReset = () => {
-        $('input[name="filtervalue"]:checkbox').prop("checked", false);
-        this.setState({ filterlist: [] });
-    };
-
-    handleFilterApply = () => {
-        var tmpList = $('input[name="filtervalue"]:checkbox:checked')
-            .map(function () {return $(this).val();}).get(); // <----
-        this.setState({ filterlist: tmpList });
-    };
-
-    handleFilterIcon = () => {
-        $(".filterboxwrapper").toggle();
-    };
-
-
-    render() {
-
-        // if (this.state.isLoaded === false){
-        //      return <Loadingicon/>
-        // }
-		  if ("msg" in this.props.userInfo){  // TODO: Is "msg" ever included with successful user login?
-            // Redirect to "login" if user isn't logged in upon arrival
-            return <Redirect to="/login" />
-    	  }
-          const credentials = localStorage.getItem('userCredentials');
-          if (credentials === null) {
-            console.log("Credentials missing from user info " + JSON.stringify(this.props.userInfo))
-            window.location.href = this.props.initObj["webroot"] + "/login";
-          }
-        // console.log("Rendering file list. User info is\n" + JSON.stringify(this.props.userInfo))
-
-		  var filObjOne = filterObjectList(this.state.objlist, this.state.filterlist);
-      var passedObjList = filObjOne.passedobjlist;
-      var passedCount = passedObjList.length;
-      //var filterInfo = filObjOne.filterinfo;
-
-      var filObjTwo = filterObjectList(passedObjList, []);
-      var filterInfo = filObjTwo.filterinfo;
-
-      var batchSize = 20;
-      var pageCount = parseInt(passedObjList.length / batchSize) + 1;
-      const len = this.state.objlist ? this.state.objlist.length : 0
-      pageCount = (len > 0 ? pageCount : 0);
-
-      var startIdx = batchSize * (parseInt(this.state.pageIdx) - 1) + 1;
-      var endIdx = startIdx + batchSize;
-      endIdx = (endIdx > passedCount ? passedCount : endIdx);
-
-      var filterHideFlag = "block";
-
-      var tmpList = [];
-      for (var i in this.state.filterlist) {
-        var h = "<b>" + this.state.filterlist[i].split("|")[1] + "</b>";
-        tmpList.push(h);
-      }
-      var resultSummary = ""
-      // XXX console.log("State obj: " + JSON.stringify(passedObjList))
-      if (this.state.statobj && "total" in this.state.statobj) {
-        resultSummary = "<b>" + this.state.statobj.total + "</b> items found";
-        if (tmpList.length > 0) {
-          resultSummary += ", <b>" + passedObjList.length + "</b> shown after filters: ";
-          resultSummary += "'" + tmpList.join("', '") + "'"
-        }
-        //resultSummary += ".";
-      } else {
-        resultSummary += "<b> No </b> items found"
-      }
-
-        var tableId = "tableone";
-        var idField = "filename";
-        var tableCols = getColumns(tableId, this.props.initObj);
-        var tableRows = [];
-        // console.log("---> Building table. Total file count: " + passedObjList.length)
-        for (var i in passedObjList){
-            var obj = passedObjList[i];
-            var o = {};
-            // console.log("Exploring tableCol object: " + JSON.stringify(obj))
-          for (var j in tableCols){
-            // XXX console.log("tableCols indexed item? ---> " + JSON.stringify(tableCols[j]))
-            var f = (tableCols[j]["field"] === "id" ? idField : tableCols[j]["field"])
-            // XXX console.log("Pulled 'f' value: " + JSON.stringify(f))
-            o[tableCols[j]["field"]] = obj[f]
-          }
-          o["detail"] =  { "bcoid": obj["bcoid"], "description": obj["usability_domain"]};
-          for(const idx in obj["files_represented"]) {
-            // Deep copies to provide unique rows per filename
-            const clonedObject = JSON.parse(JSON.stringify(o))
-            const fileName = obj["files_represented"][idx]
-            // console.log("---> Found file name " + fileName)
-            clonedObject["id"] = fileName
-            tableRows.push(clonedObject)
-          }
-        }
-
-        if (!this.state.objlist) {
-          return (
-            <div>
-                <Alertdialog dialog={this.state.dialog} onClose={this.handleDialogClose} />
-					 <div className="searchboxwrapper">
-                    <Searchbox initObj={this.props.initObj} 
-                        searchquery={this.state.searchquery}
-                        onSearch={this.handleSearch} 
-                        onKeyPress={this.handleKeyPress}
-                    />
-                </div>
-               
-                <div className="material-icons rightblock filtericoncn" onClick={this.handleFilterIcon}>tune</div>
-                <div className="statscn"> 
-						  <Markup content={resultSummary}/> 
-                </div>
-                <div className="searchresultscn">
-                    <Tableview cols={tableCols} rows={tableRows} />
-                </div>
-            </div>
-        )
-        }
-
-      return (
-        <div>
-          <Alertdialog dialog={this.state.dialog} onClose={this.handleDialogClose} />
-          <div className="searchboxwrapper">
-            <Searchbox initObj={this.props.initObj}
-              searchquery={this.state.searchquery}
-              onSearch={this.handleSearch}
-              onKeyPress={this.handleKeyPress}
-            />
-          </div>
-
-          <div className="material-icons rightblock filtericoncn" onClick={this.handleFilterIcon}>tune</div>
-          <div className="statscn">
-            <Markup content={resultSummary} />
-          </div>
-          <div className="filterboxwrapper">
-            <Filter
-              filterinfo={filterInfo}
-              filterlist={this.state.filterlist}
-              resultcount={this.state.objlist.length}
-              resultSummary={resultSummary}
-              handleFilterApply={this.handleFilterApply}
-              handleFilterReset={this.handleFilterReset}
-            />
-          </div>
-          <div className="searchresultscn">
-            <Tableview cols={tableCols} rows={tableRows} />
-          </div>
-        </div>
-      );
-    }
+  const result = await response.json()
+  // console.log("Got result: " + JSON.stringify(result))
+  setState({...state, objlist: result.recordlist, statobj: result.stats})
 }
 
-export default DatasetBrowse;
+const handleDialogClose = () => {
+  // var tmpState = state;
+  // tmpState.dialog.status = false;
+  // setState(tmpState);
+  console.log("TODO ...")
+}
+
+const handleKeyPress = (e) => {
+  if(e.key === "Enter"){
+    e.preventDefault();
+    handleSearch();
+  }
+}
+
+
+const handleSearch = () => {
+  const searchQuery = ($("#query").val() === undefined ? state.searchquery : $("#query").val());
+    updateData(searchQuery)
+}
+
+const handleFilterReset = () => {
+  $('input[name="filtervalue"]:checkbox').prop("checked", false);
+  setState({ filterlist: [] });
+};
+
+const handleFilterApply = () => {
+    var tmpList = $('input[name="filtervalue"]:checkbox:checked')
+        .map(function () {return $(this).val();}).get(); // <----
+    setState({ filterlist: tmpList });
+};
+
+const handleFilterIcon = () => {
+    $(".filterboxwrapper").toggle();
+};
+
+
+  // if (state.isLoaded === false){
+  //      return <Loadingicon/>
+  // }
+// if ("msg" in props.userInfo){  // TODO: Is "msg" ever included with successful user login?
+//       // Redirect to "login" if user isn't logged in upon arrival
+//       return <Redirect to="/login" />
+//   }
+    const credentials = userCredentials
+    // const credentials = localStorage.getItem('userCredentials');
+    if (!credentials.access_token) {
+      console.log("Credentials missing from user info " + JSON.stringify(storedUserInfo))
+      window.location.href = props.initObj["webroot"] + "/login";
+    }
+  // console.log("Rendering file list. User info is\n" + JSON.stringify(props.userInfo))
+
+var filObjOne = filterObjectList(state.objlist, state.filterlist);
+var passedObjList = filObjOne.passedobjlist;
+var passedCount = passedObjList.length;
+//var filterInfo = filObjOne.filterinfo;
+
+var filObjTwo = filterObjectList(passedObjList, []);
+var filterInfo = filObjTwo.filterinfo;
+
+var batchSize = 20;
+var pageCount = parseInt(passedObjList.length / batchSize) + 1;
+const len = state.objlist ? state.objlist.length : 0
+pageCount = (len > 0 ? pageCount : 0);
+
+var startIdx = batchSize * (parseInt(state.pageIdx) - 1) + 1;
+var endIdx = startIdx + batchSize;
+endIdx = (endIdx > passedCount ? passedCount : endIdx);
+
+var filterHideFlag = "block";
+
+var tmpList = [];
+for (var i in state.filterlist) {
+  var h = "<b>" + state.filterlist[i].split("|")[1] + "</b>";
+  tmpList.push(h);
+}
+var resultSummary = ""
+// XXX console.log("State obj: " + JSON.stringify(passedObjList))
+if (state.statobj && "total" in state.statobj) {
+  resultSummary = "<b>" + state.statobj.total + "</b> items found";
+  if (tmpList.length > 0) {
+    resultSummary += ", <b>" + passedObjList.length + "</b> shown after filters: ";
+    resultSummary += "'" + tmpList.join("', '") + "'"
+  }
+  //resultSummary += ".";
+} else {
+  resultSummary += "<b> No </b> items found"
+}
+
+  var tableId = "tableone";
+  var idField = "filename";
+  var tableCols = getColumns(tableId, props.initObj);
+  var tableRows = [];
+  // console.log("---> Building table. Total file count: " + passedObjList.length)
+  for (var i in passedObjList){
+      var obj = passedObjList[i];
+      var o = {};
+      // console.log("Exploring tableCol object: " + JSON.stringify(obj))
+    for (var j in tableCols){
+      // XXX console.log("tableCols indexed item? ---> " + JSON.stringify(tableCols[j]))
+      var f = (tableCols[j]["field"] === "id" ? idField : tableCols[j]["field"])
+      // XXX console.log("Pulled 'f' value: " + JSON.stringify(f))
+      o[tableCols[j]["field"]] = obj[f]
+    }
+    o["detail"] =  { "bcoid": obj["bcoid"], "description": obj["usability_domain"]};
+    for(const idx in obj["files_represented"]) {
+      // Deep copies to provide unique rows per filename
+      const clonedObject = JSON.parse(JSON.stringify(o))
+      const fileName = obj["files_represented"][idx]
+      // console.log("---> Found file name " + fileName)
+      clonedObject["id"] = fileName
+      tableRows.push(clonedObject)
+    }
+  }
+
+  if (!state.objlist) {
+    return (
+      <div>
+          {/* <Alertdialog dialog={state.dialog} onClose={handleDialogClose} /> */}
+      <div className="searchboxwrapper">
+              <Searchbox initObj={props.initObj} 
+                  searchquery={state.searchquery}
+                  onSearch={handleSearch} 
+                  onKeyPress={handleKeyPress}
+              />
+          </div>
+          
+          <div className="material-icons rightblock filtericoncn" onClick={handleFilterIcon}>tune</div>
+          <div className="statscn"> 
+        <Markup content={resultSummary}/> 
+          </div>
+          <div className="searchresultscn">
+              <Tableview cols={tableCols} rows={tableRows} />
+          </div>
+      </div>
+  )
+  }
+
+return (
+  <div>
+    {/* <Alertdialog dialog={state.dialog} onClose={handleDialogClose} /> */}
+    <div className="searchboxwrapper">
+      <Searchbox initObj={props.initObj}
+        searchquery={state.searchquery}
+        onSearch={handleSearch}
+        onKeyPress={handleKeyPress}
+      />
+    </div>
+
+    <div className="material-icons rightblock filtericoncn" onClick={handleFilterIcon}>tune</div>
+    <div className="statscn">
+      <Markup content={resultSummary} />
+    </div>
+    <div className="filterboxwrapper">
+      <Filter
+        filterinfo={filterInfo}
+        filterlist={state.filterlist}
+        resultcount={state.objlist.length}
+        resultSummary={resultSummary}
+        handleFilterApply={handleFilterApply}
+        handleFilterReset={handleFilterReset}
+      />
+    </div>
+    <div className="searchresultscn">
+      <Tableview cols={tableCols} rows={tableRows} />
+    </div>
+  </div>
+);
+}
+
+// export default DatasetBrowse;
