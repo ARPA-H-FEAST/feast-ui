54f13c7eae606c1a85a4fa625b3ffd6068f374b1
Pat McNeely <pmcneely@gwu.edu>
2025-05-28 15:34:42 -0400

    Refactor dataset detail to implement functional component, rather than class. Implement use of global state within dataset detail.
    Also, shim in "query" for eventual dataset query support.

--- src/components/dataset_detail.jsx
+++ src/components/dataset_detail.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom/cjs/react-router-dom";
 import Alertdialog from './dialogbox';
 import Loadingicon from "./loading_icon";
 import * as LocalConfig from "./local_config";
@@ -16,37 +17,45 @@ import {sortReleaseList, downloadFile} from "./util";
 import download from "downloadjs";
 //import axios from 'axios';
 
+import { useUserStore } from "../store/userStore";
 
 
-class DatasetDetail extends Component {
-	state = {
+
+export default function DatasetDetail(props) {
+
+  const [state, setState] = useState({
    	ver:"",
     	fileobjlist:[],
 		bco:{},
     	tabidx:"bcoview",
     	dialog:{ status:false, msg:""
-    	}
-  	};
+    	}    
+  })
 
-  handleDialogClose = () => {
-    var tmpState = this.state;
-    tmpState.dialog.status = false;
-    this.setState(tmpState);
-  }
-
-
-
-  componentDidMount() {
-    var rowList = (this.props.rowList === undefined ? [] : this.props.rowList);
+  const userCredentials = useUserStore((state) => state.userCredentials)
+  const userIDTokenValues = useUserStore((state) => state.userIDTokenDetails)
+  const storedUserInfo = useUserStore((state) => state.userInfo)
+  
+  useEffect( () => {
+    var rowList = (props.rowList === undefined ? [] : props.rowList);
     var reqObj = { 
-      bcoid:this.props.bcoId 
+      bcoid: props.bcoId 
     };
-    this.fetchPageData(reqObj); 
+    fetchPageData(reqObj); 
+  }, [])  
+	
+  if (!userCredentials || !userCredentials.access_token) {
+    console.log("No user credentials located, redirecting")
+    // This doesn't work yet, but at least renders *something*
+    // see https://stackoverflow.com/a/45090151 for a possible workaround
+    return <Redirect to='/login' />
   }
 
+  const handleDialogClose = () => {
+    setState({...state, dialog: {status: false}});
+  }
 
-
-  fetchPageData = async (reqObj) => {
+  const fetchPageData = async (reqObj) => {
 
 		// let access_csrf = localStorage.getItem("access_csrf")
     // console.log("---> Getting details for BCO. reqObj: " + JSON.stringify(reqObj))
@@ -68,7 +77,8 @@ class DatasetDetail extends Component {
     }
     const result = await response.json()
     // console.log("---> Data View: Got fileobjlist " + JSON.stringify(result.fileobjlist))
-    this.setState({
+    setState(
+    {...state,
       response: "success",
       bco: result.bco,
       fileobjlist: result.fileobjlist,
@@ -78,19 +88,19 @@ class DatasetDetail extends Component {
 
 
 
- 	handleDownloadFileOld = (e) => {
+ 	const handleDownloadFileOld = (e) => {
     	e.preventDefault();
 		var fileName = e.target.id;
 		var reqObj = {"filename":fileName};
 	   downloadFile(reqObj);
  	}
 
-	handleDownloadFile = (e) => {
+	const handleDownloadFile = (e) => {
       e.preventDefault();
       var fileName = e.target.id;
       var reqObj = {
         "filename": e.target.id,
-        "bcoid": this.props.bcoId,
+        "bcoid": props.bcoId,
       };
 		
 		const access_csrf = localStorage.getItem("access_csrf")
@@ -110,7 +120,7 @@ class DatasetDetail extends Component {
       console.log("---> Downloading file by name " + fileName)
 		 		download(new Blob([result]), fileName);
 		 	}, 
-		 	(error) => { this.setState({isLoaded: true,error,}); }
+		 	(error) => { setState({isLoaded: true, dialog: error}) }
 		);		
 
 
@@ -119,111 +129,103 @@ class DatasetDetail extends Component {
 
 
 
- 	handleDownloadBco = (e) => {
+ 	const handleDownloadBco = (e) => {
    	e.preventDefault();
-    	var buffer = JSON.stringify(this.state.bco, null, 2);
-    	download(new Blob([buffer]), this.props.bcoId + ".json", "text/plain");
+    	var buffer = JSON.stringify(state.bco, null, 2);
+    	download(new Blob([buffer]), props.bcoId + ".json", "text/plain");
  	}
 
 
 
-  handleTitleClick = (e) => {
-
-    var tmpState = this.state;
-    tmpState.tabidx = e.target.id.split("-")[0];
-    this.setState(tmpState);
+  const handleTitleClick = (e) => {
+    setState({...state, tabidx: e.target.id.split("-")[0]});
   };
 
-
-
-
-
-
-  render() {
-
-    
-	if (!("response" in this.state)){
-      return <Loadingicon/>
-	}
-	if ("msg" in this.props.userInfo){
-		//alert("msg in userinfo: forwarding");
-		window.location.href = this.props.initObj["webroot"] + "/login";
-	}
-
-
- 
-    var tabHash = {};
-    tabHash["bcoview"] = {title:"BCO JSON"}
-    tabHash.bcoview.cn = (<div style={{fontSize:"14px"}}><pre>{JSON.stringify(this.state.bco, null, 2)}</pre></div>);
-    
-	var downloadItems = [];
-	for (var i in this.state.fileobjlist){
-		var fileObj = this.state.fileobjlist[i];
-    var fileName = fileObj["filename"];
-		downloadItems.push(
-			<li>
-			<Link id={fileName} to={fileName} bco={this.state.bco} className="reglink" onClick={this.handleDownloadFile}>{fileName}</Link>
-			</li>);
-  	}
-
-	tabHash["downloads"] = {title:"DOWNLOADS",cn:(<ul style={{margin:"20px 0px 100px 20px"}}>{downloadItems}</ul>)};
-
-    var tabTitleList= [];
-    var tabContentList = [];
-    for (var tabId in tabHash){
-        var activeFlag = (tabId === this.state.tabidx ? "active" : "" );
-        var btnStyle = {width:"100%", fontSize:"15px", color:"#333", border:"1px solid #ccc"};
-        btnStyle.color = (activeFlag === "active" ? "#990000" : "#333");
-        btnStyle.background = (activeFlag === "active" ? "#fff" : "#eee");
-        btnStyle.borderBottom = (activeFlag === "active" ? "1px solid #fff" : "1px solid #ccc");
-        tabTitleList.push(
-          <li key={"tab-"+tabId} className="nav-item" role="presentation" 
-            style={{width:"20%"}}>
-            <button className={"nav-link " + activeFlag} 
-            id={tabId + "-tab"}  data-bs-toggle="tab" 
-            data-bs-target={"#sample_view"} type="button" role="tab" aria-controls={"sample_view-cn"} aria-selected="true"
-            style={btnStyle} onClick={this.handleTitleClick}
-            >
-             {tabHash[tabId].title}  
-            </button>
-        </li>
-      );
-      tabContentList.push(
-        <div key={"formcn-"+tabId} 
-          className={"tab-pane fade show  leftblock " + activeFlag} 
-          id={tabId+"-cn"} role="tabpanel" aria-labelledby={tabId + "-tab"}
-          style={{width:"100%",  padding:"20px", background:"#fff"}}>
-          {tabHash[tabId].cn}
-        </div>
-      );
-    }
-
-
- 	var linkObjList = [
-		{"url":this.props.initObj["webroot"] + "/browse", "lbl":"BROWSE"},
-		{"url":this.props.initObj["webroot"] + "/detail/" + this.props.bcoId, "lbl":this.props.bcoId}
-	];
-   //<div className="leftblock" style={{width:"100%", margin:"40px 0px 0px 0px"}}> </div>
-   return (
-      <div className="leftblock" style={{width:"100%"}}>
-		<Nav linkObjList={linkObjList}/>
-		<div className="pagecn" style={{width:"90%", marginLeft:"5%", marginRight:"5%"}}>
-        <Alertdialog dialog={this.state.dialog} onClose={this.handleDialogClose}/>
-        <div className="leftblock" 
-          style={{width:"100%", margin:"20px 0px 0px 0px"}}>
-          <ul className="nav nav-tabs" id="myTab" role="tablist" key="tabList"
-            style={{width:"70%", border:"0px dashed orange"}}>
-            {tabTitleList}
-          </ul>
-          <div className="tab-content" id="myTabContent" 
-            style={{width:"100%", margin:"20px 0px 0px 0px"}}>
-            {tabContentList}
-          </div>
-        </div>
-      </div>
-		</div>
-    );
-  }
+if (!("response" in state)){
+    return <Loadingicon/>
+}
+const credentials = userCredentials
+// const credentials = localStorage.getItem('userCredentials');
+if (!credentials.access_token) {
+  console.log("Credentials missing from user info " + JSON.stringify(storedUserInfo))
+  window.location.href = props.initObj["webroot"] + "/login";
 }
 
-export default DatasetDetail;
+
+
+  var tabHash = {};
+  tabHash["bcoview"] = {title:"BCO JSON"}
+  tabHash.bcoview.cn = (<div style={{fontSize:"14px"}}><pre>{JSON.stringify(state.bco, null, 2)}</pre></div>);
+  
+var downloadItems = [];
+for (var i in state.fileobjlist){
+  var fileObj = state.fileobjlist[i];
+  var fileName = fileObj["filename"];
+  downloadItems.push(
+    <li>
+    <Link id={fileName} to={fileName} bco={state.bco} className="reglink" onClick={handleDownloadFile}>{fileName}</Link>
+    </li>);
+  }
+
+tabHash["downloads"] = {title:"DOWNLOADS",cn:(<ul style={{margin:"20px 0px 100px 20px"}} key="downloads">{downloadItems}</ul>)};
+
+tabHash["query"] = {
+  title: "QUERY",
+  cn: (<div></div>)
+}
+
+var tabTitleList= [];
+var tabContentList = [];
+for (var tabId in tabHash){
+    var activeFlag = (tabId === state.tabidx ? "active" : "" );
+    var btnStyle = {width:"100%", fontSize:"15px", color:"#333", border:"1px solid #ccc"};
+    btnStyle.color = (activeFlag === "active" ? "#990000" : "#333");
+    btnStyle.background = (activeFlag === "active" ? "#fff" : "#eee");
+    btnStyle.borderBottom = (activeFlag === "active" ? "1px solid #fff" : "1px solid #ccc");
+    tabTitleList.push(
+      <li key={"tab-"+tabId} className="nav-item" role="presentation" 
+        style={{width:"20%"}}>
+        <button className={"nav-link " + activeFlag} 
+        id={tabId + "-tab"}  data-bs-toggle="tab" 
+        data-bs-target={"#sample_view"} type="button" role="tab" aria-controls={"sample_view-cn"} aria-selected="true"
+        style={btnStyle} onClick={handleTitleClick}
+        >
+          {tabHash[tabId].title}  
+        </button>
+    </li>
+  );
+  tabContentList.push(
+    <div key={"formcn-"+tabId} 
+      className={"tab-pane fade show  leftblock " + activeFlag} 
+      id={tabId+"-cn"} role="tabpanel" aria-labelledby={tabId + "-tab"}
+      style={{width:"100%",  padding:"20px", background:"#fff"}}>
+      {tabHash[tabId].cn}
+    </div>
+  );
+}
+
+var linkObjList = [
+  {"url":props.initObj["webroot"] + "/browse", "lbl":"BROWSE"},
+  {"url":props.initObj["webroot"] + "/detail/" + props.bcoId, "lbl":props.bcoId}
+];
+  //<div className="leftblock" style={{width:"100%", margin:"40px 0px 0px 0px"}}> </div>
+  return (
+    <div className="leftblock" style={{width:"100%"}}>
+  <Nav linkObjList={linkObjList}/>
+  <div className="pagecn" style={{width:"90%", marginLeft:"5%", marginRight:"5%"}}>
+      {/* <Alertdialog dialog={state.dialog} onClose={handleDialogClose}/> */}
+      <div className="leftblock" 
+        style={{width:"100%", margin:"20px 0px 0px 0px"}}>
+        <ul className="nav nav-tabs" id="myTab" role="tablist" key="tabList"
+          style={{width:"70%", border:"0px dashed orange"}}>
+          {tabTitleList}
+        </ul>
+        <div className="tab-content" id="myTabContent" 
+          style={{width:"100%", margin:"20px 0px 0px 0px"}}>
+          {tabContentList}
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
